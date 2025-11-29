2fe92456d7bbc7a0a738783825f34ceb99528cca
Miraku17 <zhaztedv@gmail.com>
2025-11-29 19:43:47 +0800

    reverted config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: false, // Disable Turbopack to fix CJS module issues
-  },
+  /* config options here */
 };
 
 export default nextConfig;
